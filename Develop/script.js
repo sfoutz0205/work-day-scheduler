94ec3e81f51fddcd39e33ee9fb3eaa85c6a20af6
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-08-30 18:39:21 -0600

    store and retrieve tasks

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
 // Display current date in header
 $('#currentDay').html(moment().format('MMMM DD, YYYY'));
 
+// add Hour tracker to identify past, present & future
 function hourTracker() {
-    //get current number of hours.
     var currentHour = moment().hour();
 
     
@@ -21,4 +21,26 @@ function hourTracker() {
     })
 }
 
+
+  //  save tasks to local storage
+  $(".saveBtn").on("click", function() {
+    var value = $(this).siblings(".task-input").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, value);
+  });
+
+// retrieve tasks from local storage
+  $("#hour9 .task-input").val(localStorage.getItem("hour9"));
+  $("#hour10 .task-input").val(localStorage.getItem("hour10"));
+  $("#hour11 .task-input").val(localStorage.getItem("hour11"));
+  $("#hour12 .task-input").val(localStorage.getItem("hour12"));
+  $("#hour13 .task-input").val(localStorage.getItem("hour13"));
+  $("#hour14 .task-input").val(localStorage.getItem("hour14"));
+  $("#hour15 .task-input").val(localStorage.getItem("hour15"));
+  $("#hour16 .task-input").val(localStorage.getItem("hour16"));
+  $("#hour17 .task-input").val(localStorage.getItem("hour17"));
+
+
 hourTracker();
+
+
